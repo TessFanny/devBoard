d3dcb283eb5d297136b0880352d0d75fed443ffb
Kxkee <tom.cousin06@gmail.com>
2023-02-21 19:01:10 +0100

    1. Trying to reach github/login/oauth with clientID on front

--- DevBoard/src/components/Register/Register.jsx
+++ DevBoard/src/components/Register/Register.jsx
@@ -97,12 +97,7 @@ export default function Register() {
   };
 
   const HandleGitHubAuth = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/auth/github');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENTID}`);
   };
   return (
     <Flex
