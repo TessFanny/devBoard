771a1e8ca8f61b896d155e14a4b6ca05f05140ea
rachid-madani <cryptomadani.nexo@gmail.com>
2023-02-21 02:22:29 +0100

    ajout navigation sidebar

--- DevBoard/src/components/App/App.jsx
+++ DevBoard/src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
 import Login from '../Login/Login';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <div className="app">
       <Login />
+      <NotFound />
     </div>
+
   );
 }
 
--- DevBoard/src/components/NotFound/NotFound.jsx
+++ DevBoard/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 function NotFound() {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            <p>La page que vous recherchez n'existe pas...</p>
+            <p>The page you are looking for does not exist.</p>
         </div>
     );
 }
