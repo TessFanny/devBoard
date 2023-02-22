e5fe1c3a605624d8f69070ac76b76ae4b0b435de
Julien <julien.esnau@gmail.com>
2023-02-22 16:46:54 +0100

    Created Sidebar component

--- DevBoard/src/components/App/App.jsx
+++ DevBoard/src/components/App/App.jsx
@@ -1,29 +1,11 @@
 import { useState } from 'react';
 import { Box, useBreakpointValue } from '@chakra-ui/react';
-
-import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 
-const smVariant = { navigation: 'drawer', navigationButton: true };
-const mdVariant = { navigation: 'sidebar', navigationButton: false };
-
 export default function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
-
-  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
-
   return (
     <>
-      <Sidebar
-        variant={variants?.navigation}
-      />
-      <Box ml={!variants?.navigationButton && 200}>
-        <Header
-          showSidebarButton={variants?.navigationButton}
-          onShowSidebar={toggleSidebar}
-        />
-      </Box>
+      <Sidebar />
     </>
   );
 }
--- DevBoard/src/components/Sidebar/Sidebar.jsx
+++ DevBoard/src/components/Sidebar/Sidebar.jsx
@@ -1,56 +1,84 @@
 import React from 'react';
 import {
   Box,
+  Flex,
+  Spacer,
+  useDisclosure,
+  Text,
   Button,
-  Drawer,
-  DrawerOverlay,
-  DrawerCloseButton,
-  DrawerHeader,
-  DrawerBody,
-  DrawerContent,
-  VStack,
 } from '@chakra-ui/react';
+import {
+  FaChartLine,
+  FaNewspaper,
+  FaComments,
+  FaFolderOpen,
+  FaColumns,
+  FaThumbsUp,
+  FaUser,
+} from 'react-icons/fa';
 
-const SidebarContent = ({ onClick }) => (
-  <VStack>
-    <Button onClick={onClick} w="100%">
-      Home
-    </Button>
-    <Button onClick={onClick} w="100%">
-      About
-    </Button>
-    <Button onClick={onClick} w="100%">
-      Contact
-    </Button>
-  </VStack>
-);
-
-const Sidebar = ({ variant }) => {
-  return variant === 'sidebar' ? (
+const Sidebar = ({ isOpen, onClose }) => {
+  return (
     <Box
-      position="fixed"
-      left={0}
-      p={5}
-      w="200px"
-      top={0}
-      h="100%"
-      bg="#dfdfdf"
+      bg="gray.200"
+      w="64"
+      h="full"
+      pos="fixed"
+      top="0"
+      left="0"
+      overflowY="auto"
+      transition="ease-in-out .2s"
+      transform={isOpen ? 'translateX(0)' : '-translateX(100%)'}
     >
-      <SidebarContent />
+      <Flex p="4" alignItems="center">
+        <Text fontSize="xl" fontWeight="bold">
+          DevBoard
+        </Text>
+      </Flex>
+      <Box
+        p="4"
+        pl="30"
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+      >
+        <Button variant="ghost" mb="2" leftIcon={<FaChartLine />}>
+          Dashboard
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaNewspaper />}>
+          News
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaComments />}>
+          Forum
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaFolderOpen />}>
+          Your projects
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaColumns />}>
+          Kanban
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaThumbsUp />}>
+          Likes
+        </Button>
+        <Button variant="ghost" mb="2" leftIcon={<FaUser />}>
+          Profile
+        </Button>
+      </Box>
     </Box>
-  ) : (
-    <Drawer placement="left">
-      <DrawerOverlay>
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Chakra-UI</DrawerHeader>
-          <DrawerBody>
-            <SidebarContent />
-          </DrawerBody>
-        </DrawerContent>
-      </DrawerOverlay>
-    </Drawer>
   );
 };
 
-export default Sidebar;
+const Layout = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Sidebar isOpen={true} onClose={onClose} />
+      <Box ml="64" p="4">
+        {children}
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
