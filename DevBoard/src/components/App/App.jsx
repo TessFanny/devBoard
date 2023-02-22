a814eaec4a805691ac5d2b8f747b74dee7578829
Julien <julien.esnau@gmail.com>
2023-02-22 23:10:07 +0100

    Add font-family Outfit to root div

--- DevBoard/src/components/App/App.jsx
+++ DevBoard/src/components/App/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import { Box, useBreakpointValue } from '@chakra-ui/react';
 import Sidebar from '../Sidebar/Sidebar';
-import Layout from '../Sidebar/Sidebar';
-
 export default function App() {
   return (
     <>
--- DevBoard/src/components/Sidebar/Sidebar.jsx
+++ DevBoard/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Sidebar.scss';
 import {
   Box,
   Flex,
