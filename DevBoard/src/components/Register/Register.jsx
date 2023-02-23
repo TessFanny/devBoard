7089b9243e6cf4bdce3ef1a07446740b9a1ce027
Julien <julien.esnau@gmail.com>
2023-02-23 10:36:39 +0100

    Added github oAuth

--- DevBoard/src/components/Register/Register.jsx
+++ DevBoard/src/components/Register/Register.jsx
@@ -12,13 +12,16 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  Link,
   Center,
   FormErrorMessage,
-} from '../../import/import';
-import { useState, useDispatch, useSelector } from '../../import/import';
-import { FaGithub } from '../../import/import';
-import { ViewIcon, ViewOffIcon } from '../../import/import';
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import {
   changeEmailValue,
   changePasswordValue,
@@ -28,12 +31,56 @@ import {
 } from '../../features/register/register';
 // Hooks for state management
 export default function Register() {
+  const [rerender, setRerender] = useState(false);
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get('code');
+
+    if (codeParam && localStorage.getItem('accessToken') === null) {
+      async function getAccessToken() {
+        const response = await fetch(
+          'http://localhost:3000/getAccessToken?code=' + codeParam
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            if (data.access_token) {
+              localStorage.setItem('accessToken', data.access_token);
+              setRerender(!rerender);
+              getUserData();
+            }
+          });
+      }
+      getAccessToken();
+    }
+  }, []);
+
+  async function getUserData() {
+    const response = await fetch('http://localhost:3000/getUserData', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('accessToken'), // Bearer ACCESSTOKEN
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   const [showPassword, setShowPassword] = useState(false);
   const [isEmailValid, setEmailValid] = useState(true);
   const [isPasswordValid, setPasswordValid] = useState(true);
   const [isPasswordConfirmed, setPasswordConfirmed] = useState(true);
   const [isUsernameValid, setUsernameValid] = useState(true);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // Selector from store
   const { username, email, password, confirmPassword } = useSelector(
     (state) => state.register
@@ -49,41 +96,54 @@ export default function Register() {
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    // Define a switch statement to handle form validation
-    switch (true) {
-      // If the username field is empty, set the username validation to false and return
-      case username.trim() === '':
-        setUsernameValid(false);
-        return;
-      // If the email field is not valid according to the email regex, set the email validation to false and return
-      case !emailRegex.test(email):
-        setUsernameValid(true);
-        setEmailValid(false);
-        return;
-      // If the password field is not valid according to the password regex, set the password validation to false and return
-      case !passwordRegex.test(password):
-        setEmailValid(true);
-        setPasswordValid(false);
-        return;
-      // If the password and confirmPassword fields do not match, set the password confirmation validation to false and return
-      case password !== confirmPassword:
-        setPasswordValid(true);
-        setPasswordConfirmed(false);
-        return;
-      // If all the validations pass, set all validation states to true, and dispatch the registerUser action with the form data
-      default:
-        setUsernameValid(true);
-        setEmailValid(true);
-        setPasswordValid(true);
-        setPasswordConfirmed(true);
-        dispatch(registerUser({ username, email, password }));
+    if (username.trim() === '') {
+      setUsernameValid(false);
+      return;
     }
+    // Email muist contain something ans pass the regex
+    if (!emailRegex.test(email)) {
+      setEmailValid(false);
+      return;
+    }
+
+    if (!passwordRegex.test(password)) {
+      setPasswordValid(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setPasswordConfirmed(false);
+      return;
+    }
+
+    setUsernameValid(true);
+    setEmailValid(true);
+    setPasswordValid(true);
+    setPasswordConfirmed(true);
+
+    dispatch(registerUser({ username, email, password }));
+    navigate('/');
   };
-  // Cette fonction prend deux arguments : dispatch et actionCreator
-  // Elle renvoie une nouvelle fonction qui sera utilisée dans l'onChange
-  // Cette nouvelle fonction prend un argument (evt) et utilise dispatch et actionCreator pour créer et envoyer une action Redux
-  const handleInputChange = (dispatch, actionCreator) => (evt) => {
-    dispatch(actionCreator(evt.target.value));
+  const handleUsernameChange = (evt) => {
+    dispatch(changeUsernameValue(evt.target.value));
+  };
+  const handlePasswordChange = (evt) => {
+    dispatch(changePasswordValue(evt.target.value));
+  };
+
+  const handleConfirmPasswordChange = (evt) => {
+    dispatch(changeConfirmPasswordValue(evt.target.value));
+  };
+
+  const handleEmailChange = (evt) => {
+    dispatch(changeEmailValue(evt.target.value));
+    setEmailValid(true);
+  };
+
+  const HandleGitHubAuth = async () => {
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+    );
   };
   return (
     <Flex
@@ -92,7 +152,7 @@ export default function Register() {
       justify="center"
       bg={useColorModeValue('gray.50', 'gray.800')}
     >
-      <Stack spacing={8} mx="1" maxW="lg" px={6}>
+      <Stack spacing={8} mx="1" maxW="lg" py={12} px={6}>
         <Stack align="center">
           <Heading fontSize="4xl" textAlign="center">
             Sign up
@@ -110,13 +170,7 @@ export default function Register() {
           <Stack spacing={4}>
             <FormControl id="firstName" isRequired isInvalid={!isUsernameValid}>
               <FormLabel mx="auto">Username</FormLabel>
-              {/* // Utilisation de handleInputChange pour créer une fonction de rappel onChange pour les composants d'entrée
-                  // La valeur de l'entrée est extraite de l'événement et utilisée pour créer une action Redux qui est envoyée via le store */}
-              <Input
-                type="text"
-                onChange={handleInputChange(dispatch, changeUsernameValue)}
-                mr="32"
-              />
+              <Input type="text" onChange={handleUsernameChange} mr="32" />
               {!isUsernameValid && (
                 <FormErrorMessage>Username is required.</FormErrorMessage>
               )}
@@ -128,7 +182,7 @@ export default function Register() {
                 type="email"
                 placeholder="Devboarduser@email.com"
                 value={email}
-                onChange={handleInputChange(dispatch, changeEmailValue)}
+                onChange={handleEmailChange}
                 required
               />
               {!isEmailValid && (
@@ -141,7 +195,7 @@ export default function Register() {
               <InputGroup>
                 <Input
                   type={showPassword ? 'text' : 'password'}
-                  onChange={handleInputChange(dispatch, changePasswordValue)}
+                  onChange={handlePasswordChange}
                 />
                 <InputRightElement h="full">
                   <Button
@@ -173,10 +227,7 @@ export default function Register() {
               <InputGroup>
                 <Input
                   type={showPassword ? 'text' : 'password'}
-                  onChange={handleInputChange(
-                    dispatch,
-                    changeConfirmPasswordValue
-                  )}
+                  onChange={handleConfirmPasswordChange}
                 />
                 <InputRightElement h="full">
                   <Button
@@ -218,6 +269,7 @@ export default function Register() {
                   bg: 'black.500',
                   border: 'transparent',
                 }}
+                onClick={HandleGitHubAuth}
               >
                 <Center>
                   <Text>Sign in with Github</Text>
@@ -226,10 +278,7 @@ export default function Register() {
             </Stack>
             <Stack pt={6}>
               <Text align="center">
-                Already a user?{' '}
-                <Link href="/login" color="blue.400">
-                  Login
-                </Link>
+                Already a user? <Link to="/login">Login</Link>
               </Text>
             </Stack>
           </Stack>
