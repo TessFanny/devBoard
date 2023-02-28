4acfc4306c72f134c6df7066986f09e23f902f63
Kxkee <tom.cousin06@gmail.com>
2023-02-28 18:12:31 +0100

    Homepage code cleaned

--- DevBoard/src/components/Homepage/Homepage.jsx
+++ DevBoard/src/components/Homepage/Homepage.jsx
@@ -15,18 +15,39 @@ function Homepage() {
     },
   };
   return (
-      <Flex bgGradient='linear(to-r, #2e76ff, #172c69)' w="100%" minH="100%" borderRadius="md" boxShadow="md" flexDirection={['column', 'column','column', 'row']}>
-          <Image src={blob2} position="absolute" opacity="0.5" w={['80%', '80%', '80%', '35%']} left="-50" top="-70"></Image>
-        <Image src={blob} position="fixed" top="-500" right="-710" opacity="0.1"></Image>
-        <Box w={['100%', '100%', '100%', '50%']} h={['50%', '50%', '50%', '100%']} display="flex" justifyContent={['center', 'center', 'center', 'flex-end']} alignItems="center" pr={['0', '0', '0', '10']}>
+      <Flex bgGradient='linear(to-r, #2e76ff, #172c69)'
+            w="100%" minH="100%"
+            flexDirection={['column', 'column','column', 'row']}>
+
+          <Image src={blob2}
+                 position="absolute"
+                 opacity="0.5"
+                 w={['80%', '80%', '80%', '35%']}
+                 left="-50"
+                 top="-70">
+          </Image>
+
+        <Image src={blob}
+               position="fixed"
+               top="-500"
+               right="-710"
+               opacity="0.1">
+        </Image>
+
+        <Box w={['100%', '100%', '100%', '50%']}
+             h={['50%', '50%', '50%', '100%']}
+             display="flex"
+             justifyContent={['center', 'center', 'center', 'flex-end']}
+             alignItems="center"
+             pr={['0', '0', '0', '10']}>
+
           <Box display="flex"
                flexDirection="column"
                w={['90%', '90%', '80%', '60%']} h="50%"
                alignItems="center"
                justifyContent={['flex-start', 'flex-start', 'flex-end', 'center']}
-               mb="10"
+               mb="10">
 
-          >
             <Text
                 fontSize="70px"
                 fontWeight="700"
@@ -35,20 +56,29 @@ function Homepage() {
                 mb="5">
               Devboard
             </Text>
+
             <Text fontSize="lg"
                   textAlign="center"
-                  color="gray.300"
-            >
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galle
+                  color="gray.300">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galle
             </Text>
+
               <Link to="/register">
-            <Button w={['150px', '150px', '200px', '150px']} mt="6" colorScheme="blue"
+            <Button w={['150px', '150px', '200px', '150px']}
+                    mt="6" colorScheme="blue"
             >Start here
             </Button>
               </Link>
           </Box>
         </Box>
-        <Box w={['100%', '100%', '100%', '50%']} h={['50%', '50%', '50%', '100%']} display="flex" alignItems={['flex-end', 'center', 'center']} justifyContent={['center', 'center', 'center', 'flex-start']} pl={['0', '0', '0', '10']}>
+        <Box w={['100%', '100%', '100%', '50%']}
+             h={['50%', '50%', '50%', '100%']}
+             display="flex"
+             alignItems={['flex-end', 'center', 'center']}
+             justifyContent={['center', 'center', 'center', 'flex-start']}
+             pl={['0', '0', '0', '10']}>
 
           <Lottie
               animationData={homepage}
