f8752cc008c5aac9d7146dee993e2e0919c9d85f
Julien <julien.esnau@gmail.com>
2023-03-01 09:55:56 +0100

    added commentary in Stackoverflow jsx component and fixed widght bug on a sidebar

--- DevBoard/src/components/StackOverflowSearch/StackOverflowSearch.jsx
+++ DevBoard/src/components/StackOverflowSearch/StackOverflowSearch.jsx
@@ -13,32 +13,36 @@ import {
   SimpleGrid,
   Heading,
 } from '@chakra-ui/react';
-
+// This function component is called StackOverflowSearch
 function StackOverflowSearch() {
+  // The state variables 'query', 'results' and 'loading' are declared using the useState hook
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  // This function is used to search StackOverflow using the 'query' state variable
   const searchStackOverflow = async () => {
     setLoading(true);
     const url = `https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=${query}&site=stackoverflow`;
     const response = await fetch(url);
     const data = await response.json();
+    // The 'results' state variable is updated with the data obtained from the search
     setResults(data.items);
     setLoading(false);
   };
-
+  // This function is called whenever the value of the search input changes
   const handleQueryChange = (event) => {
+    // The 'query' state variable is updated with the new value of the input
     setQuery(event.target.value);
   };
-
+  // This function is called when the search form is submitted
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    // If the 'query' state variable is not empty, then the 'searchStackOverflow' function is called
     if (query.trim() !== '') {
       searchStackOverflow();
     }
   };
-
+  // The component returns JSX that renders a search form and displays the search results
   return (
     <Flex
       w="90%"
@@ -51,10 +55,12 @@ function StackOverflowSearch() {
       flexDir="column"
       align="center"
     >
+      {/* /* This is the search form / */}
       <form
         onSubmit={handleFormSubmit}
         style={{ width: '90%', marginBottom: '2rem' }}
       >
+        {/* {/ The search input and submit button are wrapped in a Stack /} */}
         <Stack direction="row" align="center">
           <Input
             type="text"
@@ -76,6 +82,8 @@ function StackOverflowSearch() {
           </Button>
         </Stack>
       </form>
+      {/* If the search is currently loading, a Spinner and a Text component are displayed  */}
+
       {loading ? (
         <Flex justify="center" align="center" w="90%">
           <Stack align="center">
@@ -83,7 +91,8 @@ function StackOverflowSearch() {
             <Text>Searching...</Text>
           </Stack>
         </Flex>
-      ) : results.length > 0 ? (
+      ) : /* // If the search has returned results, they are displayed in a Box component */
+      results.length > 0 ? (
         <Box style={{ flex: '1', width: '95%', h: '75%' }}>
           <Stack spacing={2} w="95%" h="92%" overflowY="scroll">
             {results.map((result) => (
