cbf5848208e4d73104eec420206ea592405ed6af
Julien <julien.esnau@gmail.com>
2023-03-03 09:38:26 +0100

    new stack overflow

--- DevBoard/src/components/StackOverflowSearch/StackOverflowSearch.jsx
+++ DevBoard/src/components/StackOverflowSearch/StackOverflowSearch.jsx
@@ -13,6 +13,7 @@ import {
   SimpleGrid,
   Heading,
 } from '@chakra-ui/react';
+import { TiInputChecked } from 'react-icons/ti';
 // This function component is called StackOverflowSearch
 function StackOverflowSearch() {
   // The state variables 'query', 'results' and 'loading' are declared using the useState hook
@@ -22,7 +23,7 @@ function StackOverflowSearch() {
   // This function is used to search StackOverflow using the 'query' state variable
   const searchStackOverflow = async () => {
     setLoading(true);
-    const url = `https://api.stackexchange.com/2.3/search?order=desc&sort=relevance&intitle=${query}&site=stackoverflow&pagesize=20`;
+    const url = `https://api.stackexchange.com/2.3/search?order=desc&sort=relevance&intitle=${query}&site=stackoverflow&pagesize=20&filter=withbody`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
@@ -45,46 +46,48 @@ function StackOverflowSearch() {
   };
   // The component returns JSX that renders a search form and displays the search results
   return (
-    <Flex
-      w="98%"
-      minH="80%"
-      mt="10"
-      bgColor="gray.50"
-      borderRadius="md"
-      boxShadow="md"
-      p="4"
-      flexDir="column"
-      align="center"
-    >
-      {/* /* This is the search form / */}
-      <form
-        onSubmit={handleFormSubmit}
-        style={{ width: '90%', marginBottom: '2rem' }}
+    <>
+      <Flex
+        w="98%"
+        minH="80%"
+        mt="10"
+        bgColor="gray.50"
+        borderRadius="md"
+        boxShadow="md"
+        p="4"
+        flexDir="column"
+        align="center"
       >
-        {/* {/ The search input and submit button are wrapped in a Stack /} */}
-        <Stack direction="row" align="center">
-          <Input
-            type="text"
-            placeholder="Search Stack Overflow"
-            value={query}
-            onChange={handleQueryChange}
-            size="lg"
-            focusBorderColor="blue.500"
-            _hover={{ borderColor: 'blue.500' }}
-          />
-          <Button
-            type="submit"
-            colorScheme="blue"
-            size="lg"
-            isLoading={loading}
-            loadingText="Searching"
-          >
-            Search
-          </Button>
-        </Stack>
-      </form>
-      {/* If the search is currently loading, a Spinner and a Text component are displayed  */}
+        {/* /* This is the search form / */}
+        <form
+          onSubmit={handleFormSubmit}
+          style={{ width: '90%', marginBottom: '2rem' }}
+        >
+          {/* {/ The search input and submit button are wrapped in a Stack /} */}
+          <Stack direction="row" align="center">
+            <Input
+              type="text"
+              placeholder="Search Stack Overflow"
+              value={query}
+              onChange={handleQueryChange}
+              size="lg"
+              focusBorderColor="blue.500"
+              _hover={{ borderColor: 'blue.500' }}
+            />
+            <Button
+              type="submit"
+              colorScheme="blue"
+              size="lg"
+              isLoading={loading}
+              loadingText="Searching"
+            >
+              Search
+            </Button>
+          </Stack>
+        </form>
+        {/* If the search is currently loading, a Spinner and a Text component are displayed  */}
 
+<<<<<<< HEAD
       {loading ? (
         <Flex justify="center" align="center" w="90%">
           <Stack align="center">
@@ -150,6 +153,107 @@ function StackOverflowSearch() {
         </Flex>
       )}
     </Flex>
+=======
+        {loading ? (
+          <Flex justify="center" align="center" w="90%">
+            <Stack align="center">
+              <Spinner size="lg" />
+              <Text>Searching...</Text>
+            </Stack>
+          </Flex>
+        ) : /* // If the search has returned results, they are displayed in a Box component */
+        results.length > 0 ? (
+          <Box style={{ flex: '1', width: '100%', h: '75%' }}>
+            <Stack display="flex" w="100%" h="92%" overflowY="scroll">
+              {results.map((result) => (
+                <div key={result.question_id}>
+                  <Link href={result.link} isExternal>
+                    <Box boxShadow="md" p={4} borderRadius="md">
+                      <Flex alignItems="center">
+                        <Box>
+                          <Text
+                            fontWeight="bold"
+                            fontSize="sm"
+                            style={{
+                              display: 'flex',
+                              margin: '0vw',
+                              padding: '2px',
+                              justifyContent: 'flex-end',
+                            }}
+                          >
+                            {result.score} votes
+                          </Text>
+
+                          <Text
+                            fontSize="sm"
+                            bgColor="green"
+                            color="white"
+                            style={{
+                              display: 'flex',
+                              flexDirection: 'row',
+                              alignItems: 'center',
+                              padding: '2px',
+                            }}
+                          >
+                            <TiInputChecked
+                              size={30}
+                              style={{ marginRight: '0.5rem' }}
+                            />
+                            {result.answer_count} answers
+                          </Text>
+
+                          <Text
+                            fontSize="sm"
+                            style={{
+                              display: 'flex',
+                              margin: '0vw',
+                              padding: '2px',
+                              justifyContent: 'flex-end',
+                            }}
+                          >
+                            {result.view_count} views
+                          </Text>
+                        </Box>
+                        <Heading size="md" pl="10px">
+                          {result.title}
+                          <Text
+                            style={{
+                              paddingTop: '2rem',
+                            }}
+                          >
+                            {result.body
+                              .replace(/<\/?p>/g, '')
+                              .split('\n')
+                              .slice(0, 2)
+                              .join('\n')}
+                            {result.body.replace(/<\/?p>/g, '').split('\n')
+                              .length > 2 && '...'}
+                          </Text>
+                        </Heading>
+                      </Flex>
+                      <Box />
+                      <Text mb={2}>By {result.owner.display_name}</Text>
+                      <List display="flex" flexWrap="wrap" mb={2}>
+                        {result.tags.map((tag, index) => (
+                          <ListItem key={index} mr={2}>
+                            {tag}
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Box>
+                  </Link>
+                </div>
+              ))}
+            </Stack>
+          </Box>
+        ) : (
+          <Flex justify="center" align="center" w="100%" mt="4">
+            <Text>Make a research.</Text>
+          </Flex>
+        )}
+      </Flex>
+    </>
+>>>>>>> d5c647a (Commit v1 of new box stackoverflow return)
   );
 }
 
