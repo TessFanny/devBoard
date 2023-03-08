e4b2c222663cc70fa10b224bde0dc212820b7941
Kxkee <tom.cousin06@gmail.com>
2023-03-08 13:06:43 +0100

    playground style

--- DevBoard/src/components/Playground/playground.jsx
+++ DevBoard/src/components/Playground/playground.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import useLocalStorage from './useLocalStorage'
-import './index.css';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import {html} from '@codemirror/lang-html'
 import { css } from '@codemirror/lang-css';
 import { okaidia } from '@uiw/codemirror-theme-okaidia';
-import { Button } from '@chakra-ui/react';
+import {Box, Button, Flex, useMediaQuery} from '@chakra-ui/react';
 import { FaCopy } from 'react-icons/fa';
 
 function Playground() {
@@ -14,8 +13,11 @@ function Playground() {
   const [cssdata, setCss] = useLocalStorage('css', '')
   const [js, setJs] = useLocalStorage('js', '')
   const [srcDoc, setSrcDoc] = useState('')
+    const [isSmallerThan1000] = useMediaQuery('(max-width: 1000px)');
+    const [isSmallerThan1200] = useMediaQuery('(max-width: 1200px)');
 
-  useEffect(() => {
+
+    useEffect(() => {
     const timeout = setTimeout(() => {
       setSrcDoc(`
         <html>
@@ -35,63 +37,83 @@ function Playground() {
   };
 
   return (
-    <>
-      <div className="pane">
-       <div className='top-pane'>
-       <div>
-       <CodeMirror
-          value={js}
-          placeholder='Please enter the JavaScript code.'
-          height="100%"
-          width="400px"
-          theme={okaidia}
-          extensions={[javascript({ jsx: true })]}      
-          onChange={setJs}
-        />
-        <Button onClick={() => copyToClipboard(js)}><FaCopy />  Copy
-        </Button>
-        </div>
-        <div>
-          <CodeMirror
-            value={htmldata}
-            placeholder='Please enter the Html code.'
-            height="100%"
-            width="400px"
-            theme={okaidia}
-            extensions={[html({
-              matchClosingTags: true
-            })]}      
-            onChange={setHtml}
-          />
-          <Button onClick={() => copyToClipboard(htmldata)}> <FaCopy /> Copy</Button>
-        </div>
-        
-        <div>
-          <CodeMirror
-              placeholder='Please enter the Css code.'
-              value={cssdata}
-              height="100%"
-              width="400px"
-              theme={okaidia}
-              extensions={[css()]}    
-              onChange={setCss}
-            />
-            <Button onClick={() => copyToClipboard(cssdata)}> <FaCopy /> Copy</Button>
-        </div>
-          
-        </div>
-        <div className="botom-pane">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-same-origin"
-          width="400px"
-          height="100%"
-        />
-      </div>
-      </div>
-      
-    </>
+      <Flex w={isSmallerThan1000 ? '100%' : '98%'}
+            h="80vh"
+            mt={10}
+            flexDirection="column"
+            gap={5}
+            bgColor="bgPrimary"
+            style={{'backdrop-filter': 'blur(15px)'}}
+            borderRadius="md"
+            boxShadow="lg"
+            p="4"
+            zIndex={1}>
+
+          <Box w="100%" h="50%" display="flex" flexDirection={isSmallerThan1200 ? 'column' : 'row'}>
+                <Box h="100%" w="100%" display="flex" flexDirection="column">
+
+                      <CodeMirror
+                          value={js}
+                          placeholder='Please enter the JavaScript code.'
+                          width="100%"
+                          height={isSmallerThan1200 ? "115px" : "360px"}
+                          theme={okaidia}
+                          style={{border: "4px solid rgba(219, 231, 255, 0.2)"}}
+                          extensions={[javascript({ jsx: true })]}
+                          onChange={setJs}
+                      />
+
+
+                </Box>
+
+                  <Box h="100%" w="100%" display="flex" flexDirection="column">
+                      <CodeMirror
+                          value={htmldata}
+                          placeholder='Please enter the Html code.'
+                          height={isSmallerThan1200 ? "115px" : "360px"}
+                          width="100%"
+                          theme={okaidia}
+                          style={{border: "4px solid rgba(219, 231, 255, 0.2)"}}
+                          extensions={[html({
+                              matchClosingTags: true
+                          })]}
+                          onChange={setHtml}
+                      />
+                  </Box>
+
+                  <Box h="100%" w="100%" display="flex" flexDirection="column">
+                      <CodeMirror
+                          placeholder='Please enter the Css code.'
+                          value={cssdata}
+                          height={isSmallerThan1200 ? "115px" : "360px"}
+                          width="100%"
+                          theme={okaidia}
+                          style={{border: "4px solid rgba(219, 231, 255, 0.2)"}}
+                          extensions={[css()]}
+                          onChange={setCss}
+                      />
+                  </Box>
+
+
+          </Box>
+
+          <Box w="100%"
+               h={["40%", "40%", "40%", "40%", "40%", "40%", "50%"]}
+               mt={["20px", "20px", "20px", "20px", "20px", "20px", "0px"]}
+               display="flex"
+               alignItems="flex-end">
+                  <iframe
+                      srcDoc={srcDoc}
+                      title="output"
+                      sandbox="allow-same-origin"
+                      width="100%"
+                      height="95%"
+                      style={{backgroundColor: "#272822", border: "7px solid rgba(219, 231, 255, 0.2)", paddingTop: "10px"}}
+                  />
+          </Box>
+
+
+      </Flex>
   )
 }
 
