9bf02b1064945ef6f06f59c89341d11173f2f34d
DUPIN <maximedupin1992@gmail.com>
2023-03-12 18:35:44 +0100

    deployfinal

--- DevBoard/src/components/App/App.jsx
+++ DevBoard/src/components/App/App.jsx
@@ -44,24 +44,24 @@ export default function App() {
     if (token) {
       setIsLoggedIn(true);
     }
-    // if (
-    //   !token && 
-    //   location.pathname == '/repositories' ||
-    //   location.pathname == '/feed'  ||
-    //   location.pathname == '/profile' ||
-    //   location.pathname == '/mypost' ||
-    //   location.pathname == '/post' ||
-    //   location.pathname == '/stackoverflow' ||
-    //   location.pathname == '/editpost/:postId' ||
-    //   location.pathname == '/kanban' ||
-    //   location.pathname == '/playground' ||
-    //   location.pathname == '/likes' ||
-    //   location.pathname == '/addpost' ||
-    //   location.pathname == '/' ||
-    //   location.pathname == '/npm'      
-    // ) {
-    //   window.location.replace('/homepage');
-    // }
+    if (
+      !token && 
+      location.pathname == '/repositories' ||
+      location.pathname == '/feed'  ||
+      location.pathname == '/profile' ||
+      location.pathname == '/mypost' ||
+      location.pathname == '/post' ||
+      location.pathname == '/stackoverflow' ||
+      location.pathname == '/editpost/:postId' ||
+      location.pathname == '/kanban' ||
+      location.pathname == '/playground' ||
+      location.pathname == '/likes' ||
+      location.pathname == '/addpost' ||
+      location.pathname == '/' ||
+      location.pathname == '/npm'      
+    ) {
+      window.location.replace('/homepage');
+    }
   }, []);
   // Redirect user to register page if not logged in and not on login or homepage routes
 
@@ -69,7 +69,7 @@ export default function App() {
   const isRegisterOrLoginRouteOrHome =
     location.pathname === '/register' ||
     location.pathname === '/homepage' ||
-    location.pathname === '/posts' ||
+    location.pathname === '/login' ||
     location.pathname === '/404';
 
   const isHomepage = location.pathname === '/homepage';
